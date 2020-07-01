4ad72f30375358ccfe9f56cf7ebbed3421c5cacd
aivcode <aiv.email@yandex.ru>
2020-07-01 12:06:35 +0300

    Old-style class

--- app.js
+++ app.js
@@ -2,3 +2,17 @@
   git remote add origin https://github.com/aivcode/js-learn.git
   git push -u origin master 
 */
+
+// Prototypes
+const Animal = function(options) { // функция явл. конструктором класса
+  this.name = options.name
+  this.color = options.color
+
+  this.voice = function() {
+    console.log('Base voice from', this.name)
+  }
+}
+
+const dog = new Animal({name: 'Rex', color: '#fff'}) // создаём экземпляр класса
+dog.voice()
+// console.log(dog);
